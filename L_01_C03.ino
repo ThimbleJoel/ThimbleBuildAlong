13f087496c3e4b6eb383274bb1c5860b88732077
ThimbleJoel <99132976+ThimbleJoel@users.noreply.github.com>
2022-02-06 20:01:05 -0500

    Create L_01_C03.ino

---
+++ L_01_C03.ino
@@ -0,0 +1,45 @@
+const int ledSocket = 6;
+
+void setup() {
+  pinMode(ledSocket, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledSocket, HIGH);
+  delay(200);
+  digitalWrite(ledSocket, LOW);
+  delay(200);
+  digitalWrite(ledSocket, HIGH);
+  delay(200);
+  digitalWrite(ledSocket, LOW);
+  delay(200);
+  digitalWrite(ledSocket, HIGH);
+  delay(200);
+  digitalWrite(ledSocket, LOW);
+  delay(200);
+  digitalWrite(ledSocket, HIGH);
+  delay(600);
+  digitalWrite(ledSocket, LOW);
+  delay(200);
+  digitalWrite(ledSocket, HIGH);
+  delay(600);
+  digitalWrite(ledSocket, LOW);
+  delay(200);
+  digitalWrite(ledSocket, HIGH);
+  delay(600);
+  digitalWrite(ledSocket, LOW);
+  delay(200);
+  digitalWrite(ledSocket, HIGH);
+  delay(200);
+  digitalWrite(ledSocket, LOW);
+  delay(200);
+  digitalWrite(ledSocket, HIGH);
+  delay(200);
+  digitalWrite(ledSocket, LOW);
+  delay(200);
+  digitalWrite(ledSocket, HIGH);
+  delay(200);
+  digitalWrite(ledSocket, LOW);
+  delay(1400);
+}
+
