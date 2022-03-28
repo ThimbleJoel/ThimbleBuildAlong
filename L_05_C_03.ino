2ab50e283d2f3cc5fb08f7be1c469e9f6e951ea5
ThimbleJoel <99132976+ThimbleJoel@users.noreply.github.com>
2022-03-27 20:09:26 -0400

    Create L_05_C_03.ino

---
+++ L_05_C_03.ino
@@ -0,0 +1,58 @@
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+const int buttonSocket = 6;
+const int dialSocket = A0;
+const int buzzerSocket = 8;
+int seconds = -1;
+int dialReading;
+
+rgb_lcd lcd;
+const int colorR = 128;
+const int colorG = 128;
+const int colorB = 128;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(buttonSocket, INPUT);
+  pinMode(dialSocket, INPUT);
+  pinMode(buzzerSocket, OUTPUT);
+  lcd.begin(16, 2);
+  lcd.setRGB(colorR, colorG, colorB);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(not digitalRead(buttonSocket)){
+    dialReading = analogRead(dialSocket)/35;
+    if(seconds != 1 + dialReading){
+      seconds = 1 + dialReading;
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print(seconds);      
+    }
+  }
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("start!");
+  delay(1000);
+  for(int x = seconds; x >= 0; x --){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(x);
+    delay(1000);
+  }
+  seconds = -1;
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("done!");
+  lcd.setRGB(255, 0, 0);
+  digitalWrite(buzzerSocket, HIGH);
+  delay(250);
+  digitalWrite(buzzerSocket, LOW);
+  delay(750);
+  lcd.setRGB(colorR, colorG, colorB);
+  while(digitalRead(buttonSocket)){
+    delay(10);
+  }
+}
