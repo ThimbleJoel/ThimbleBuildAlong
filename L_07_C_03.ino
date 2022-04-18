f1a2810dfe699b396d81f7ad63f8ec75f65e33ae
ThimbleJoel <99132976+ThimbleJoel@users.noreply.github.com>
2022-04-18 10:57:54 -0400

    Create L_07_C_03.ino

---
+++ L_07_C_03.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+#include "rgb_lcd.h"
+const int buttonSocket = 4; 
+
+rgb_lcd lcd;
+int colorR = 255;
+int colorG = 255;
+int colorB = 255;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(buttonSocket, INPUT);
+  lcd.begin(16, 2); 
+  lcd.setRGB(colorR, colorG, colorB);
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(not digitalRead(buttonSocket)){
+    delay(15);
+  }
+  colorR = random(0, 51)*5;
+  colorG = random(0, 51)*5;
+  colorB = random(0, 51)*5;
+  lcd.setRGB(colorR, colorG, colorB);
+  while(digitalRead(buttonSocket)){
+    delay(15);
+  }
+}
