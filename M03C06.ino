4fccab693e65471261001b54c5a2b8c620bea5a9
ThimbleJoel <99132976+ThimbleJoel@users.noreply.github.com>
2022-11-28 21:36:41 -0500

    Create M03C06.ino

---
+++ M03C06.ino
@@ -0,0 +1,27 @@
+const int ledSocket = 6;
+const int buttonSocket = 5; 
+int brightness = 0;
+int fadeAmount = 5;
+bool runState = false;
+
+void setup() {
+  pinMode(ledSocket, OUTPUT);
+  pinMode(buttonSocket, INPUT);
+}
+
+void loop() {
+  if(runState == true){
+    analogWrite(ledSocket, brightness);
+    brightness = brightness + fadeAmount;
+    if (brightness <= 0 || brightness >= 255){
+      fadeAmount = -fadeAmount;
+  }
+  delay(50);
+  }
+  if(digitalRead(buttonSocket)){
+    runState = not runState;
+    while(digitalRead(buttonSocket)){
+      delay(10);
+    }
+  }
+}
